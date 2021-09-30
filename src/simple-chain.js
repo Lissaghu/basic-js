61de95518716d49b231041540da29efc311e9efb
Anton_Dogadin <dogadin00@gmail.com>
2021-10-01 00:04:24 +0300

    feat: make solution simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,39 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
+  chain: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    if (value === undefined) {
+      this.chain.push('( )')
+    } else {
+      this.chain.push(`( ${value} )`);
+    }
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if (0 < position && position < this.chain.length && typeof position === 'number') {
+      this.chain.splice(position - 1, 1);
+    } else {
+      this.chain = [];
+      throw new Error(`You can't remove incorrect link!`);
+    }
+    return this;
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let res = this.chain.join('~~');
+    this.chain = [];
+    return res;
   }
 };
